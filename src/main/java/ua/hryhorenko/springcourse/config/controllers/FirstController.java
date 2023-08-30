4dc7662cbbbad325226b113f5203ab1bdf966564
Andrii Hryhorenko <grigorenko584@gmail.com>
2023-08-30 23:47:05 +0300

    Пробна робота з моделью

--- src/main/java/ua/hryhorenko/springcourse/config/controllers/FirstController.java
+++ src/main/java/ua/hryhorenko/springcourse/config/controllers/FirstController.java
@@ -1,7 +1,6 @@
 package ua.hryhorenko.springcourse.config.controllers;
-
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
   @GetMapping("/hello")
   public String helloPage(@RequestParam(value = "name", required = false) String name,
-                          @RequestParam(value = "surname", required = false) String surName) {
-    System.out.println("Hello, " + name + " " + surName);
+                          @RequestParam(value = "surname", required = false) String surName,
+                          Model model) {
+//  System.out.println("Hello, " + name + " " + surName);
+    model.addAttribute("message", "Hello, " + name + " " + surName);
 
     return "first/hello";
   }
